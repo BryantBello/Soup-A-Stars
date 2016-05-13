507c07bd7f45f050cfdb90e92e66dca3a90ee3a9
bryantbello <bryant.bello@outlook.com>
2016-05-12 21:14:23 -0400

    polished up existing code

--- app/config/connection.js
+++ app/config/connection.js
@@ -21,7 +21,7 @@ var source = {
         host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
         user: 'cm0zdmk2ez4igof5',
         password: "yuhl98jsef0k7ul4",
-        database: "lghowl35ljxl5vao" 
+        database: "lghowl35ljxl5vao"
     }
 }
 
@@ -30,14 +30,14 @@ var selectedSource = source.jawsDB;
 
 // Creates mySQL connection using Sequelize
 var sequelize = new Sequelize(selectedSource.database, selectedSource.user, selectedSource.password, {
-  host: selectedSource.host,
-  dialect: 'mysql',
+    host: selectedSource.host,
+    dialect: 'mysql',
 
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  },
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    },
 
 });
 
